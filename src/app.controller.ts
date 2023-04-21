222b7a3f7d47c326cd89d510789bb4307c5cb2dd
huangzy <>
2023-04-21 11:07:57 +0800

    优化配置信息

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Get()
+  @Get('openai/chat')
   async getAsk(): Promise<string> {
     const translated = await this.appService.getOpenAIAnswer(
       '中国有多少人口？'
